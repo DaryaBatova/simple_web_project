eab6e664487594b18120a36095cbfd7198fb3833
DaryaBatova <darmorgan99@gmail.com>
2021-06-29 13:07:28 +0000

    updated the view for the URL with name 'signup'

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', question_list_new, name='new_questions'),
     path('login/', test, name='login'),
-    path('signup/', test, name='signup'),
+    path('signup/', signup, name='signup'),
     path('question/<int:id>/', question_view, name='question'),
     path('ask/', ask_add, name='ask'),
     path('popular/', question_list_popular, name='popular'),
