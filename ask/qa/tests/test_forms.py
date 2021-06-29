7748cae4489387b9b274bb5ed697c4b4764de08f
DaryaBatova <darmorgan99@gmail.com>
2021-06-29 18:18:56 +0000

    added tests for the form LoginForm

--- ask/qa/tests/test_forms.py
+++ ask/qa/tests/test_forms.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
 
 from qa.forms import (
     EMPTY_TITLE_ERROR, EMPTY_TEXT_ERROR, AskForm, AnswerForm,
-    EMPTY_USERNAME_ERROR, EMPTY_EMAIL_ERROR, EMPTY_PASSWORD_ERROR, SignupForm
+    EMPTY_USERNAME_ERROR, EMPTY_EMAIL_ERROR, EMPTY_PASSWORD_ERROR, SignupForm, LoginForm
 )
 
 from qa.models import Question, Answer
@@ -174,3 +175,44 @@ class SignupFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['username'], [f"A user with name {saved_user.username} already exists"])
         self.assertEqual(User.objects.count(), 1)
+
+
+class LoginFormTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        username = 'test-user'
+        password = make_password('12a3W@mя45')
+        User.objects.create(username=username, email='a@b.com', password=password)
+
+    def test_login_form_validation_for_blank_fields(self):
+        form = LoginForm(data={'username': '', 'password': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['username'], [EMPTY_USERNAME_ERROR])
+        self.assertEqual(form.errors['password'], [EMPTY_PASSWORD_ERROR])
+
+    def test_login_form_invalid_if_username_isnt_registered(self):
+        # test method clean
+        form_data = {'username': 'incorrect', 'password': '12a3W@mя45'}
+        form = LoginForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['__all__'], ["Username or password is incorrect"])
+
+    def test_login_form_invalid_if_password_doesnt_match_the_user(self):
+        # test method clean
+        numeric_password = '12345678'
+        form_data = {'username': 'test-user', 'password': numeric_password}
+        form = LoginForm(data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['__all__'], ["Username or password is incorrect"])
+
+    def test_login_form_valid_if_username_is_registered(self):
+        # test method clean
+        form_data = {'username': 'test-user', 'password': '12a3W@mя45'}
+        form = LoginForm(data=form_data)
+
+        self.assertTrue(User.objects.filter(username='test-user').exists())
+        user = User.objects.get(username='test-user')
+        self.assertTrue(check_password('12a3W@mя45', user.password))
+
+        self.assertTrue(form.is_valid())
