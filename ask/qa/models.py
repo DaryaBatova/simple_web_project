e9ba89023f679b9abab6422b3e0fb705858a9abf
DaryaBatova <darmorgan99@gmail.com>
2021-07-04 21:07:07 +0000

    changed the field type added_at and added a new model QuestionLikes that represents the intermediate table to manage many-to-many relationships

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,17 +15,25 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(default="", max_length=1024)
     text = models.TextField(default="")
-    added_at = models.DateField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(User, related_name='q_to_likes')
+    likes = models.ManyToManyField(User, related_name='questions',
+                                   through='QuestionLikes', through_fields=('question', 'user'))
     objects = QuestionManager()
 
     def get_absolute_url(self):
         return reverse('question', kwargs={'id': str(self.id)})
 
+
 class Answer(models.Model):
     text = models.TextField(default="")
-    added_at = models.DateField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class QuestionLikes(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question_likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='question_likes')
+    is_liked = models.BooleanField()
